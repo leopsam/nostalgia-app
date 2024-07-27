3485af4ecb4d2975e1201221d1c0cb4437145e57
leopsam <leonardo.lps@outlook.com.br>
2024-07-27 11:48:05 -0300

    feat: add fastify cors origin true

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoryRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoryRoutes)
 
